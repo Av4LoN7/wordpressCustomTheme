3b216a189393459c50350e8eeb327b84fabeecc3
Av4LoN7 <georges.garnier1@gmail.com>
2020-08-04 11:11:25 +0200

    adding single-post & 404 page

---
+++ single-post.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<?php if ( have_posts() ): while ( have_posts() ): the_post() ?>
+    <p>
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" style="width: 100%; height: auto; margin-top: 2px">
+    </p>
+    <h1><?php the_title(); ?></h1>
+    <p><?php the_content(); ?> </p>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
